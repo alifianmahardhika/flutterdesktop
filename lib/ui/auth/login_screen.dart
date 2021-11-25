46672958bce2a9875a0df3182458de42f13307fc
Alifian Mahardhika <fiansmamda@gmail.com>
2021-11-25 10:18:50 +0900

    Update login_screen.dart

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -7,23 +7,43 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-          padding: EdgeInsets.symmetric(vertical: 100.0, horizontal: 20.0),
-          child: Column(
-            children: [
-              Text('Login'),
-              TextField(
-                decoration: InputDecoration(hintText: 'User'),
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Password'),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [Text('Forgot Password')],
-              ),
-              ElevatedButton(onPressed: () {}, child: Text('Login'))
-            ],
-          )),
+        padding: EdgeInsets.symmetric(vertical: 100.0, horizontal: 80.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'User'),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Password'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  child: Text('Forgot Password'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+            Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Login'),
+                ))
+          ]
+              .map((e) => Padding(
+                    padding: EdgeInsets.only(bottom: 8.0),
+                    child: e,
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
